0413944d3f0fe8a185d9600cf71e84741639b4f1
Dmitry Moskalev <moskalev199618120@gmail.com>
2025-03-09 22:30:48 +0300

    add docker-compose and Dockerfile, change code in main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,9 @@
-import uvicorn
 from fastapi import FastAPI
 
 from app.wallet.router import router as router_wallets
 from app.users.router import router as router_users
 
-
 app = FastAPI()
 
 app.include_router(router_wallets)
 app.include_router(router_users)
-
-if __name__ == '__main__':
-    uvicorn.run("main.app", reload=True)
